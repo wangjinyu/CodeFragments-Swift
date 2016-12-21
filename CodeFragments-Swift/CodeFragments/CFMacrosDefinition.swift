ef1dee9d8ed85b37165fc7bac410e6f1a4da21d1
王金宇 <jinyuid@gmail.com>
2016-12-21 23:08:46 +0800

    屏幕相关定义

---
+++ CodeFragments-Swift/CodeFragments/CFMacrosDefinition.swift
@@ -0,0 +1,21 @@
+//
+//  CFMacrosDefinition.swift
+//  CodeFragments-Swift
+//
+//  Created by jeevan on 2016/12/20.
+//  Copyright © 2016年 jeevan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//MARK: - 屏幕相关
+
+//获取当前设备屏幕宽度
+let screen_width  : CGFloat           =   UIScreen.main.bounds.size.width
+
+//获取当前设备屏幕高度
+let screen_height : CGFloat           =   UIScreen.main.bounds.size.height
+
+//获取当前设备屏幕比例 （eg. 1x, 2x, 3x）
+let screen_scale  : CGFloat           =   UIScreen.main.scale
